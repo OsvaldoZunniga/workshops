b0971d654eed3f449dd14573fe3dd1be04f09602
OsvaldoZunniga <osvaldo66239@gmail.com>
2025-09-21 16:02:28 -0600

    PHP and XAMPP

---
+++ Intro PHP using xampp/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    $date = date('Y-m-d-i');
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> 
+        <?php echo $date ?>
+    </p>
+</body>
+</html>
