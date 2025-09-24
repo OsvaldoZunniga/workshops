d8a0bcc55d060ed05c3e0ea3c3fa7b327e0bc7f0
OsvaldoZunniga <osvaldo66239@gmail.com>
2025-09-23 19:31:16 -0600

    Renamed folders.

--- WSP-2_Intro_PHP_using_xampp/index.php
+++ WSP-2_Intro_PHP_using_xampp/index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
+    <?php
+        date_default_timezone_set('America/Costa_Rica');
+        $date = date('Y-m-d H:i:s');
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Intro PHP using XAMPP</title>
 </head>
 <body>
     <p> 
-        2025-09-16-09
+        <?php echo $date ?>
     </p>
 </body>
 </html>
